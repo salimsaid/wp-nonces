da795c32382d2607429b536d91dd7108f5ce2e25
salimsaid2 <salim@oxoafrica.co.tz>
2019-12-19 05:48:43 +0300

    Core:Add ISecurityToken interface for Token implementation Abstract the implemenation of security tokens by defining an interface to be used by all classes that need to implement a secure Token/Nonce mechanism.
    Every class implementiong Token/Nonce functionality must implement this interace.

--- src/Blog.php
+++ src/Blog.php
@@ -1,7 +1,35 @@
 <?php
+namespace nonces;
 
+/**
+ * Blog
+ *
+ * Wp-clean-nonces is A clean OOP implementation of the Wordpress Nonce library.
+ * Copyright (C) 2019  Salim Said.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * For the complete terms of the GNU General Public License, please see this URL:
+ * http://www.gnu.org/licenses/gpl-2.0.html
 
+ * @category  NA
+ * @package   NA
+ * @author    Salim Said <saliimsaid@gmail.com>
+ * @copyright 2019 Salim Said
+ * @license   GPL-2.0+ <http://www.gnu.org/licenses/gpl-2.0.html>
+ * @link      ****
+ */
 class Blog
 {
 
 }
+
+echo "Welcome to the nonces Blog<BR>";
