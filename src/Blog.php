9b3d71435c35687dfa6508f6f79c341e47cfbb62
salimsaid2 <salim@oxoafrica.co.tz>
2019-12-19 20:01:10 +0300

    Core:Populate Blog with nonce implementation examples On visiting the Blog page, you can see examples of nonces being used.
    The example covers
    1. Creating a nonce
    2. Creating a nonce for a specific url
    3. Verifying a nonce
    4. Displaying a nonce warning upon making an action on the nonce
    5. Generating secure nonce field for secure forms.
    
    It should now be easy for anyone reading using this library to
    implement this library.

--- src/Blog.php
+++ src/Blog.php
@@ -30,10 +30,90 @@ namespace nonces;
 
 if(file_exists('../vendor/autoload.php'))
     require_once '../vendor/autoload.php';
-use \nonces\WPNonce;
+
+use nonces\Utilities;
+use nonces\WPNonce;
 class Blog
 {
+    protected $nonce;
+    protected $utility;
+
+    /**
+     * Create a token for a specific action
+     *
+     * @param WPNonce   $nonce   The nonce object to be used.
+     * @param Utilities $utility The utility object to be used.
+     *
+     * @return string
+     */
+    public function __construct(WPNonce $nonce, Utilities $utility)
+    {
+        $this->nonce = $nonce;
+        $this->utility = $utility;
+    }
+
+    /**
+     * Runs and tests token creation
+     * This method does the following
+     *
+     * 1.Create Token
+     * 2. Verify Token
+     * 3. Issue warning
+     * 4. Create secure field
+     *
+     * @return void
+     */
+    public function runTokenRoutine()
+    {
+        echo $this->nonce->createTokenUrl("inpsyde.com?action=publish_postid=2", "token");
+        $this->utility->newLine(3);
+
+        $nonce = $this->nonce->createToken("travel-token");
+        echo "Created Nonce :: <b>" . $nonce . "</b>";
+        $this->utility->newLine(2);
+
+
+        $nonce = isset($_REQUEST['_wpnonce']) ? $_REQUEST['_wpnonce'] : null;
+
+
+        if (! $this->nonce->verifyToken($nonce, 'any-wp-action')) {
+            /*
+             * Nonce is not valid
+             * Ask the user to click on the link to trigger nonce verification
+             */
+            $this->utility->newLine(2);
+
+            echo ('<strong style="color: red;"><i>Nonce Verification Failed. You are not authorized to see this 🔫</i></strong>');
+            $this->utility->newLine(1);
+            $nonce2 = $this->nonce->createToken('any-wp-action');
+
+            echo "<a href='Blog.php?_wpnonce=$nonce2'>Click here to Verify NONCE</a>";
+            $this->utility->newLine(1);
+
+        } else {
+            $this->utility->newLine(2);
+            echo " Nonce verified successfully  ";
+            echo "<a href='Blog.php'>Go home</a>";
+            $this->utility->newLine(2);
+        }
+
+
+        /*
+         * Create secure form field
+         * To see this form field printed on-screen, inspect the html elements of this page on chrome
+         */
+        $this->nonce->generateSecureFormField('my-action', 'secure-field');
+
+    }
 
 }
 
-echo "Welcome to the nonces Blog<BR>";
+echo "<h2>Welcome to the nonces Blog</h2>";
+(new Utilities())->newLine(2);
+
+//display blog
+$token = new WPNonce();
+$util = new Utilities();
+
+$blog = new Blog($token, $util);
+$blog->runTokenRoutine();
